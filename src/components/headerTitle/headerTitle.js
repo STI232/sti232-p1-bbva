43b1cef40519359f965d1f3fdf3f6c435101d982
twitchprimeplease <melanieospitia@hotmail.com>
2023-09-27 16:32:35 -0500

    Componente header

--- src/components/headerTitle/headerTitle.js
+++ src/components/headerTitle/headerTitle.js
@@ -1,18 +1,27 @@
 class headerTitle extends HTMLElement {
     constructor() {
         super();
+        this.name = this.getAttribute('title');
+    }
+
+    connectedCallback(){
         this.render();
     }
+
+    static get observedAttributes(){
+        return ['title']
+    }
+    
 
     render() {
         this.innerHTML = `
         <link rel="stylesheet" href="/src/components/headerTitle/headerTitleStyles.css">
         <div class="header-title">
                     <div>
-                        <img src="../../images/arrow.png" alt="" padding-rigt="5%">
+                        <img src="../images/arrow.png" alt="" padding-rigt="5%">
                     </div>
                     <div>
-                        <h3 class="subtitle1">Cuentas compartidas</h3>
+                        <h3 class="subtitle1">${this.title}</h3>
                     </div>
                 </div>
         `;
--- src/importComponents.js
+++ src/importComponents.js
@@ -1,4 +1,5 @@
 import './components/transAmigo/transAmigo.js'
 import './components/bbvaFooter/bbvaFooter.js'
 import './components/beneficio/beneficio.js'
+import './components/headerTitle/headerTitle.js' 
 
